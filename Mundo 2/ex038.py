c22c854dd69d75e79ef14f7afe8ce9f4dcd5e4ea
Jean Luca <jean.luca162021@outlook.com>
2025-01-07 15:55:25 -0300

    new exercises

---
+++ Mundo 2/ex038.py
@@ -0,0 +1,12 @@
+
+#--- Exercício 038 ---#
+
+primeiro = int(input('\nDigite o primeiro valor: '))
+segundo = int(input('Digite o segundo valor: '))
+
+if primeiro > segundo:
+    print('\nO primeiro valor é maior')
+elif segundo > primeiro:
+    print('\nO segundo valor é maior')
+else:
+    print('\nOs dois valores são iguais')
