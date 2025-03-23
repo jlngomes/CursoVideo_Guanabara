6a8f3c84122c5b2a0b90fd8b6c9faebcf232bded
Jean Luca <jean.luca162021@outlook.com>
2025-03-23 16:54:02 -0300

    fix ex53

--- Mundo 2/ex053.py
+++ Mundo 2/ex053.py
@@ -6,10 +6,10 @@ palavras = frase.split()
 junto = ''.join(palavras)
 inverso = ''
 
-print(f'O inverso de {junto} é {inverso}')
-
 for letra in range(len(junto) -1, -1, -1):
      inverso += junto[letra]
+
+print(f'O inverso de {junto} é {inverso}')
 
 if inverso == junto:
     print('É um palíndromo está frase')
@@ -18,14 +18,14 @@ else:
 
 #--- Alternativa 1; Sem o for usando fatiamento ---#
 
-frase = str(input('Digite um frase: ')).strip().upper()
-palavras = frase.split()
-junto = ''.join(palavras)
-inverso = junto[::-1]
-
-print(f'O inverso de {junto} é {inverso}')
-
-if inverso == junto:
-    print('É um palíndromo está frase')
-else:
-    print('Não é um palíndromo está frase')
+# frase = str(input('Digite um frase: ')).strip().upper()
+# palavras = frase.split()
+# junto = ''.join(palavras)
+# inverso = junto[::-1]
+#
+# print(f'O inverso de {junto} é {inverso}')
+#
+# if inverso == junto:
+#     print('É um palíndromo está frase')
+# else:
+#     print('Não é um palíndromo está frase')
